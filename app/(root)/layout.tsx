ba411e18f1b40fe36212b2f146cd2a32bdb4e024
byronpantoja <byronpantoja@gmail.com>
2024-03-30 18:28:57 +0800

    organizing folder structure and adding dynamic routes

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import { channel } from 'diagnostics_channel'
+import React, { ReactNode } from 'react'
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      Navbar
+      {children}
+      Footer
+    </main>
+  )
+}
+
+export default RootLayout
